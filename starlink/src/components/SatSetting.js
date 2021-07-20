ff463333b46a175e86ad380edbfe63170c586440
liyang-code <liyang.code@gmail.com>
2021-07-19 21:43:17 -0700

    project initialization

---
+++ starlink/src/components/SatSetting.js
@@ -0,0 +1,112 @@
+import React, {Component} from 'react';
+import { Form, InputNumber, Button} from 'antd';
+
+class SatSettingForm extends Component {
+    render() {
+        console.log(this.props)
+        const { getFieldDecorator } = this.props.form;
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 11 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 13 },
+            },
+        };
+        return (
+            <Form {...formItemLayout} onSubmit={this.showSatellite}>
+                <Form.Item label="Longitude(degrees)">
+                    {
+                        getFieldDecorator("longitude", {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: "Please input your Longitude",
+                                }
+                            ],
+                        })(<InputNumber min={-180} max={180}
+                                        style={{width: "100%"}}
+                                        placeholder="Please input Longitude"
+                        />)
+                    }
+                </Form.Item>
+
+                <Form.Item label="Latitude(degrees)">
+                    {
+                        getFieldDecorator("latitude", {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: "Please input your Latitude",
+                                }
+                            ],
+                        })(<InputNumber placeholder="Please input Latitude"
+                                        min={-90} max={90}
+                                        style={{width: "100%"}}
+                        />)
+                    }
+                </Form.Item>
+
+                <Form.Item label="Elevation(meters)">
+                    {
+                        getFieldDecorator("elevation", {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: "Please input your Elevation",
+                                }
+                            ],
+                        })(<InputNumber placeholder="Please input Elevation"
+                                        min={-413} max={8850}
+                                        style={{width: "100%"}}
+                        />)
+                    }
+                </Form.Item>
+
+                <Form.Item label="Altitude(degrees)">
+                    {
+                        getFieldDecorator("altitude", {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: "Please input your Altitude",
+                                }
+                            ],
+                        })(<InputNumber placeholder="Please input Altitude"
+                                        min={0} max={90}
+                                        style={{width: "100%"}}
+                        /> )
+                    }
+                </Form.Item>
+
+                <Form.Item label="Duration(secs)">
+                    {
+                        getFieldDecorator("duration", {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: "Please input your Duration",
+                                }
+                            ],
+                        })(<InputNumber placeholder="Please input Duration" min={0} max={90} style={{width: "100%"}} />)
+                    }
+                </Form.Item>
+                <Form.Item className="show-nearby">
+                    <Button type="primary" htmlType="submit" style={{textAlign: "center"}}>
+                        Find Nearby Satellite
+                    </Button>
+                </Form.Item>
+            </Form>
+        );
+    }
+
+    showSatellite = e => {
+        e.preventDefault();
+        console.log('click')
+    }
+}
+
+const SatSetting = Form.create()(SatSettingForm); // this is HOC
+export default SatSetting;
